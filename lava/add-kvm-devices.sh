5ccf605f5491ba2a26c145a0f3bc95487cb2f574
hellbentv <apps@hellbentventures.com>
2014-01-17 07:33:54 -0700

    Adding lava helper functions and yaml to start populating test instance

---
+++ lava/add-kvm-devices.sh
@@ -0,0 +1,12 @@
+# Params
+#    $1 - lava instance name
+#    $2 - kvm device_type
+#    $3 - number of devices
+for i in (1..$2); do
+mkdir -p /srv/lava/instances/$1/etc/lava-dispatcher/devices
+cat > /srv/lava/instances/$1/etc/lava-dispatcher/devices/$2-i.conf <<EOL
+device_type = $2 
+root_part = 1 
+EOL
+done
+
---
+++ lava/create-superuser.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect
+# Params
+#   $1 - lava instance
+#   $2 - admin username
+#   $3 - admin password
+set instance  [lindex $argv 0]
+set adminuser [lindex $argv 1]
+set adminpass [lindex $argv 2]
+set adminemail "$adminuser@email.com"
+
+spawn /root/lava-deployment-tool/lava-deployment-tool manage $instance createsuperuser
+expect "):"
+send "$adminuser\n"
+expect "address:"
+send "$adminemail\n"
+expect "Password: "
+send "$adminpass\n"
+expect "Password (again): "
+send "$adminpass\n"
