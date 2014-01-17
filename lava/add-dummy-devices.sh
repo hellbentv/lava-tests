877a8d10556ae5fb59b4feeb8902bec6ac76cf6d
akb <akb@hellbentventures.com>
2014-01-17 15:47:15 -0700

    Update add-dummy-devices.sh

--- lava/add-dummy-devices.sh
+++ lava/add-dummy-devices.sh
@@ -9,14 +9,14 @@ driver=$4
 numdevices=$5
 
 #add a new dummy-devicetype.conf file
-mkdir -p /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types
-cat > /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types/dummy-$driver.conf <<EOL
-#/srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types/dummy-$driver.conf
-client_type = dummy
-dummy_driver = $driver
-EOL
-#show files
-head -n 1000 /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types/dummy-$driver.conf
+#mkdir -p /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types
+#cat > /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types/dummy-$driver.conf <<EOL
+##/srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types/dummy-$driver.conf
+#client_type = dummy
+#dummy_driver = $driver
+#EOL
+##show files
+#head -n 1000 /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/device-types/dummy-$driver.conf
 
 
 # Create N dummy-devicetype-driver-$i devices
