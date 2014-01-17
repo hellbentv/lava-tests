4f343b93be296fbc4e5cc5bee672d78ede7b1622
akb <akb@hellbentventures.com>
2014-01-17 08:47:59 -0700

    Update add-kvm-devices.sh

--- lava/add-kvm-devices.sh
+++ lava/add-kvm-devices.sh
@@ -2,11 +2,13 @@
 #    $1 - lava instance name
 #    $2 - kvm device_type
 #    $3 - number of devices
-for i in (1..$2); do
-mkdir -p /srv/lava/instances/$1/etc/lava-dispatcher/devices
-cat > /srv/lava/instances/$1/etc/lava-dispatcher/devices/$2-$i.conf <<EOL
-device_type = $2 
+LAVA_INSTANCE=$1
+devicetype=$2
+devicecount=$3
+for i in $(seq 1 $devicecount); do
+mkdir -p /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/devices
+cat > /srv/lava/instances/$LAVA_INSTANCE/etc/lava-dispatcher/devices/$devicetype-$i.conf <<EOL
+device_type = $devicetype
 root_part = 1 
 EOL
 done
-
