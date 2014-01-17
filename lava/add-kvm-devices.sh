bd14530b4dd6928d67f6512c5a06c8cd76a2f8c3
akb <akb@hellbentventures.com>
2014-01-17 07:40:58 -0700

    Update add-kvm-devices.sh

--- lava/add-kvm-devices.sh
+++ lava/add-kvm-devices.sh
@@ -4,7 +4,7 @@
 #    $3 - number of devices
 for i in (1..$2); do
 mkdir -p /srv/lava/instances/$1/etc/lava-dispatcher/devices
-cat > /srv/lava/instances/$1/etc/lava-dispatcher/devices/$2-i.conf <<EOL
+cat > /srv/lava/instances/$1/etc/lava-dispatcher/devices/$2-$i.conf <<EOL
 device_type = $2 
 root_part = 1 
 EOL
