b121e77b470e478c984d8e4ed5e76f624537e83e
Alan Bennett <alan.bennett@linaro.org>
2014-02-26 16:16:53 -0700

    Adding a few changes to better support the schroot.   TODO: still working on getting a schroot to execute

--- qemu/chroot.sh
+++ qemu/chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get -y install binfmt-support qemu
+apt-get -y install binfmt-support qemu schroot
 
 cat > /usr/share/binfmts/qemu-aarch64 <<EOL
 package qemu-user-static
@@ -9,12 +9,16 @@ offset 0
 magic \x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7
 mask \xff\xff\xff\xff\xff\xff\xff\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff
 EOL
+update-binfmts --import qemu-aarch64
 
-cat > /etc/schroot/schroot.conf <<EOL
-[chroot-aarch64]
+groupadd user
+useradd -g user user
+
+cat > /etc/schroot/chroot.d/schroot-aarch64.conf <<EOL
+[aarch64]
 description=arm64 chroot
-directory=/root/chroot-aarch64
-type=directory
+file=/root/debian-unstable-arm64.tar.gz
+type=file
 users=user
 groups=user,sbuild
 root-groups=user,sbuild
