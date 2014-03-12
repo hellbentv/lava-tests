74129094bcae77b7ba58710cc49f7e75bbd7da2a
akb <akb@hellbentventures.com>
2014-03-11 21:21:22 -0600

    Update secrets.sh

--- ansible/secrets.sh
+++ ansible/secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Create server.key"
-openssl genrsa -out /root/localhost.pem 2048
+openssl genrsa -out /root/lava-server.pem 2048
 
 echo 'Create secrets.yml file'
-echo 'crowd_app_name: crowd' > /root/secrets.yml
+echo 'crowd_app_name: crowdname' > /root/secrets.yml
 echo 'crowd_app_password: crowdpw' >> /root/secrets.yml
-echo 'db_password: corwdpw' >> /root/secrets.yml
+echo 'db_password: databaspw' >> /root/secrets.yml
 echo 'openssl_certs: ' >> /root/secrets.yml
-echo '    - /root/localhost.pem' >> /root/secrets.yml
+echo '    - /root/lava-server.pem' >> /root/secrets.yml
