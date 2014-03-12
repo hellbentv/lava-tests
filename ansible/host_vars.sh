f9298245212f7f275a9e3252672e82e426efc404
akb <akb@hellbentventures.com>
2014-03-11 22:13:12 -0600

    Update host_vars.sh

--- ansible/host_vars.sh
+++ ansible/host_vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Create host_vars file"
-echo 'nickname: patches' > /root/localhost
+echo 'nickname: lava-server' > /root/localhost
 echo 'hostname: lava-server' >> /root/localhost
 echo 'role: staging' >> /root/localhost
 echo 'patchwork_repos_path: /opt/patchwork' >> /root/localhost
