d7d03add91a997544f19e3a2eec3a85172a69957
akb <akb@hellbentventures.com>
2014-03-11 22:56:40 -0600

    Update secrets.sh

--- ansible/secrets.sh
+++ ansible/secrets.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-echo "Create server.key"
-openssl genrsa -out /root/lava-server.pem 2048
+cd /root
+
+echo "Create server.key & new lava-server.csr"
+openssl req -new -x509 -newkey rsa:2048 -nodes -out lava-server.csr -keyout lava-server.key -days 365 \
+    -subj "/C=US/ST=Arizona/L=Scottsdale/O=lavaserver.com/CN=lava-server"
+
+echo "TODO: Do we need a task?"
+cat lava-server.key lava-server.csr > lava-server.pem
 
 echo 'Create secrets.yml file'
 echo 'crowd_app_name: crowdname' > /root/secrets.yml
@@ -8,3 +14,5 @@ echo 'crowd_app_password: crowdpw' >> /root/secrets.yml
 echo 'db_password: databaspw' >> /root/secrets.yml
 echo 'openssl_certs: ' >> /root/secrets.yml
 echo '    - /root/lava-server.pem' >> /root/secrets.yml
+echo '    - /root/lava-server.key' >> /root/secrets.yml
+echo '    - /root/lava-server.csr' >> /root/secrets.yml
